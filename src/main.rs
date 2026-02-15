45d58b219ecf1e245418d4880c89162d08515cd8
VJ-2303 <vanaraj1018@gmail.com>
2026-02-15 15:01:24 +0530

    extract query and filename into variables

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("We received these arguments: {:?}", args);
+
+    if args.len() < 3 {
+        panic!("Not enough arguments provided");
+    }
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
