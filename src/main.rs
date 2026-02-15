48f8c1f9fe882bc95bff791a90ca836cf07557e4
VJ-2303 <vanaraj1018@gmail.com>
2026-02-15 15:22:02 +0530

    implemented line-by-line search

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,15 @@ fn main() {
     let query = &args[1];
     let filename = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    println!("Searching for: {}", query);
+    println!("In file: {}", filename);
+    println!("--- Results ---");
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            println!("{}", line)
+        }
+    }
 }
