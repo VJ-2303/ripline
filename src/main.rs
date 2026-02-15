1c7636d432fa510451392cd2e4959fc30f6692d1
VJ-2303 <vanaraj1018@gmail.com>
2026-02-15 15:13:06 +0530

    read file content into string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,4 +12,8 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
